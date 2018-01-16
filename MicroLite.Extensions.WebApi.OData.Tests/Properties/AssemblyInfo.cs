da8bfd79d58b32b979f4f193bfa79bdea34763d8
Trevor Pilley <trev@trevorpilley.co.uk>
2018-01-16 14:21:40 +0000

    Updated assembly info file version

--- MicroLite.Extensions.WebApi.OData.Tests/Properties/AssemblyInfo.cs
+++ MicroLite.Extensions.WebApi.OData.Tests/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using Xunit;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.6.0.0")]
-[assembly: AssemblyFileVersion("6.6.1.0")]
+[assembly: AssemblyFileVersion("6.6.2.0")]
 [assembly: CollectionBehavior(DisableTestParallelization = false)]
